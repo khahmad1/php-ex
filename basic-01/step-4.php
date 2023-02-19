107f47227d90f67c4761a93b20c14530b381bf45
Ahmad  Kharma <ahmad2003kharma@gmail.com>
2023-02-19 22:02:51 +0200

    step 4

---
+++ basic-01/step-4.php
@@ -0,0 +1,7 @@
+<?php
+$name = "ahmad";
+$position= "junior developer";
+$github_url="https://github.com/khahmad1";
+echo "Hello I 'm $name I'm $position please check my github link $github_url <br> ";
+echo "Hello, I'm ", $name, " I'm a ", $position, ", please check my github link ", $github_url ,".<br>";
+echo 'Hello, I\'m ' . $name .'  I\'m a ' . $position . ', please check my github link ' . $github_url . '.<br>';
