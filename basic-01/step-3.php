df3da24cdd32a29755636047a4b39e957d1f6e34
Ahmad  Kharma <ahmad2003kharma@gmail.com>
2023-02-19 11:43:59 +0200

    step 3

---
+++ basic-01/step-3.php
@@ -0,0 +1,8 @@
+<?php
+$name ='ahmad';
+$email='ahmad2003kharma@gmail.com';
+echo "My name is $name and my email is $email";
+$height = 170;
+$width=70;
+$area= $height*$width;
+echo "the area its  $area ";
