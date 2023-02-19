53652497fbe7e56062cb50ddf7ba50293e486757
Ahmad  Kharma <ahmad2003kharma@gmail.com>
2023-02-19 22:11:14 +0200

    step 5

---
+++ basic-01/step-5.php
@@ -0,0 +1,9 @@
+<?php
+$course_name = "Rust, the language of no bugs";
+$enrolled_students = 20;
+$price = 30.0;
+$on_discount = false;
+echo "Course title: $course_name<br>";
+echo "Enrolled Students: $enrolled_students<br>";
+echo "Course price: $price  USD<br>";
+echo "Course on discount:"  .($on_discount ? "Yes":"No").  "<br>";
