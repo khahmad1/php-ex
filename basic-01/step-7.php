c0d50dc8a3b398aaf5a7d2f45103c31d00fca5eb
Ahmad  Kharma <ahmad2003kharma@gmail.com>
2023-02-19 22:17:59 +0200

    step 7

---
+++ basic-01/step-7.php
@@ -0,0 +1,8 @@
+<?php
+$birth_year = 2003;
+$current_year = date('Y');
+if ($current_year - $birth_year > 18){
+    echo "You can drive";
+} else {
+    echo "You are still a kid, go and play GTA";
+}
