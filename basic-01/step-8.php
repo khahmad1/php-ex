e70d5a97871e0d66e3c118c92404675df9e99baa
Ahmad  Kharma <ahmad2003kharma@gmail.com>
2023-02-19 22:25:38 +0200

    step 8b

--- basic-01/step-8.php
+++ basic-01/step-8.php
@@ -3,3 +3,10 @@ $grocery = array('Eggs', 'Milk', 'Cheese', 'Water Pack', 'Tissues', 'Watermelon'
 $first_three_items = array_slice($grocery, 0, 3);
 $last_item = end($grocery);
 echo $message = 'Hello Sir, do you have ' . implode(', ', $first_three_items) . '? Also if you sell fruits can I find a ' . $last_item . '? <br>';
+$items = array('eggs' => array('balade', 'mazere3'),
+    'milk' => array('Fresh', 'Taanayel'),
+    'water-pack' => array('Tanoureen', 'Reem')
+  );
+  $egg_brand = $items['eggs'][0];
+  $water_pack_brand = $items['water-pack'][1];
+  echo $message = "Hey Sir, Please I need 1 pack of $egg_brand eggs and 3 $water_pack_brand Water Pack.";
